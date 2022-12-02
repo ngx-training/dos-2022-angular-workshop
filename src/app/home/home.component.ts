64c15fdb689ef2cc1b6dadfb6f2f2c7d1889b6f6
Gregor Doroschenko <g.doroschenko@gmail.com>
2022-12-02 15:00:29 +0100

    Add load posts request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+import { Post } from '../services/post.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  // posts: Post[] = [];
+  posts$!: Observable<Post[]>;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.loadPosts();
+
+    /*this.postService.loadPosts().subscribe(result => {
+      this.posts = result;
+    }, error => {
+      console.error(error);
+    });*/
+  }
 
 }
